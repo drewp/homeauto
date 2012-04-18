6ef266aff9452691c834c7197186a7a71d3521ce
drewp@bigasterisk.com <drewp@bigasterisk.com>
2012-04-17 21:49:32 -0700

    barcode player send commands to pympd Ignore-this: 8500b91697c9b77ea1e24009f062d61

--- service/starArduino/barcodePlayer.py
+++ service/starArduino/barcodePlayer.py
@@ -5,31 +5,39 @@ receives POSTs about barcodes that are scanned, plays songs on mpd
 
 from __future__ import division
 
-import cyclone.web, cyclone.httpclient, sys, json
+import cyclone.web, cyclone.httpclient, sys, json, urllib
 from twisted.python import log
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks
 sys.path.append("/my/proj/homeauto/lib")
 from cycloneerr import PrettyErrorHandler
-
-
+from pymongo import Connection
+mpdPaths = Connection("bang", 27017)['barcodePlayer']['mpdPaths']
 
 class Index(PrettyErrorHandler, cyclone.web.RequestHandler):
     def get(self):
-        
-        self.set_header("Content-Type", "application/xhtml+xml")
-        self.write(open("index.html").read())
+        self.write("barcode player. POST to /barcodeScan")
 
 class BarcodeScan(PrettyErrorHandler, cyclone.web.RequestHandler):
     @inlineCallbacks
     def post(self):
-        print json.loads(self.request.body)
+        code = json.loads(self.request.body)
 
-        song = "cd/Kindermusik-The_Best_of_the_Best/14.Fiddle-de-dee.ogg"
+        if not code['code'].startswith("music "):
+            raise ValueError("this service only knows music barcodes, not %r" %
+                             code)
 
-        print (yield cyclone.httpclient.fetch(url="http://star:9009/addAndPlay/%s" % song, method="POST")).body
+        rows = list(mpdPaths.find({'_id' : int(code['code'].split()[1])}))
+        if not rows:
+            raise ValueError("code %r unknown" % code)
 
-        self.write("ok")
+        song = rows[0]['mpdPath']
+
+        post = "http://star:9009/addAndPlay/%s" % urllib.quote(song, safe='')
+        result = (yield cyclone.httpclient.fetch(
+            method="POST", url=post)).body
+        print result
+        self.write(result)
 
 
 if __name__ == '__main__':
