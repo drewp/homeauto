ae920a29d787f15c7b77b65162b6abfd2716602d
drewp@bigasterisk.com <drewp@bigasterisk.com>
2013-02-10 12:06:17 -0800

    wallscreen switch to html5 and cyclone. add temporary temperature poller Ignore-this: 2606b551325933d2a42b14292605b967

--- service/wallscreen/wallscreen.py
+++ service/wallscreen/wallscreen.py
@@ -4,78 +4,91 @@ for raspberry pi screen.
 and then fix the window with this:
   echo "window.resizeTo(702,480)" | nc localhost 9999
 """
-import bottle, json, pystache, restkit
+import json, sys
 from dateutil.parser import parse
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+import cyclone.web, cyclone.httpclient, cyclone.websocket
 from rdflib import Graph, URIRef, Namespace, Literal, RDF
+
+sys.path.append("../../lib")
+from logsetup import log
+from cycloneerr import PrettyErrorHandler
+
 CV = Namespace("http://bigasterisk.com/checkvist/v1#")
 EV = Namespace("http://bigasterisk.com/event#")
 
-@bottle.route('/')
-def index():
-    return pystache.render(open("vist.html").read())
+class Content(PrettyErrorHandler, cyclone.web.RequestHandler):
+    def get(self):
+        out = []
+        if 0: # needs to be rewritten for trello
+            g = Graph()
+            g.parse("http://bang:9103/graph", format="n3")
 
-@bottle.route("/content")
-def content():
-
-    out = []
-    if 0: # needs to be rewritten for trello
-        g = Graph()
-        g.parse("http://bang:9103/graph", format="n3")
-
-        tasks = [] # (pos, task)
-        for t in g.subjects(RDF.type, CV.OpenTask):
-            if (None, CV.child, t) in g:
-                continue
-            tasks.append((g.value(t, CV.position), t))
-        tasks.sort()
-
-        def appendTree(t, depth):
-            out.append(dict(
-                uri=t,
-                depth=depth,
-                mark=g.value(t, CV.mark),
-                content=g.value(t, CV.content),
-                ))
-            for sub in g.objects(t, CV.child):
-                if (sub, RDF.type, CV.OpenTask) not in g:
+            tasks = [] # (pos, task)
+            for t in g.subjects(RDF.type, CV.OpenTask):
+                if (None, CV.child, t) in g:
                     continue
-                appendTree(sub, depth+1)
+                tasks.append((g.value(t, CV.position), t))
+            tasks.sort()
 
-        for pos, t in tasks[:10]:
-            appendTree(t, depth=0)
+            def appendTree(t, depth):
+                out.append(dict(
+                    uri=t,
+                    depth=depth,
+                    mark=g.value(t, CV.mark),
+                    content=g.value(t, CV.content),
+                    ))
+                for sub in g.objects(t, CV.child):
+                    if (sub, RDF.type, CV.OpenTask) not in g:
+                        continue
+                    appendTree(sub, depth+1)
 
-    events = [] # [{'date':'yyyy-mm-dd', 'dayEvents':[], 'timeEvents':[]}]
-    g = Graph()
-    g.parse("http://bang:9105/events?days=3", format='n3')
-    byDay = {}
-    for ev in g.subjects(RDF.type, EV.Event):
-        start = g.value(ev, EV['start'])
-        s = parse(start)
-        d = s.date().isoformat()
-        byDay.setdefault(d, {'dayEvents':[],
-                             'timeEvents':[]})[
-            'timeEvents' if 'T' in start else 'dayEvents'].append({
-            'title' : g.value(ev, EV['title']),
-            'start' : start,
-            'date' : s.date().isoformat(),
-            'time' : s.time().isoformat()[:-3],
-            })
-    for k,v in sorted(byDay.items(), key=lambda (k,v): k):
-        d = {'date':k, 'weekdayName':parse(k).strftime("%A")}
-        d.update(v)
-        d['dayEvents'].sort(key=lambda ev: ev['title'])
-        d['timeEvents'].sort(key=lambda ev: ev['start'])
-        events.append(d)
-        
-    return {'tasks':out, 'events' : events}
+            for pos, t in tasks[:10]:
+                appendTree(t, depth=0)
 
-@bottle.route("/thermostat")
-def thermostat():
-    return restkit.request("http://bang:10001/requestedTemperature").body_string()
+        events = [] # [{'date':'yyyy-mm-dd', 'dayEvents':[], 'timeEvents':[]}]
+        g = Graph()
+        g.parse("http://bang:9105/events?days=3", format='n3')
+        byDay = {}
+        for ev in g.subjects(RDF.type, EV.Event):
+            start = g.value(ev, EV['start'])
+            s = parse(start)
+            d = s.date().isoformat()
+            byDay.setdefault(d, {'dayEvents':[],
+                                 'timeEvents':[]})[
+                'timeEvents' if 'T' in start else 'dayEvents'].append({
+                'title' : g.value(ev, EV['title']),
+                'start' : start,
+                'date' : s.date().isoformat(),
+                'time' : s.time().isoformat()[:-3],
+                })
+        for k,v in sorted(byDay.items(), key=lambda (k,v): k):
+            d = {'date':k, 'weekdayName':parse(k).strftime("%A")}
+            d.update(v)
+            d['dayEvents'].sort(key=lambda ev: ev['title'])
+            d['timeEvents'].sort(key=lambda ev: ev['start'])
+            events.append(d)
+
+        self.write(json.dumps({'tasks':out, 'events' : events}))
+
+class Thermostat(PrettyErrorHandler, cyclone.web.RequestHandler):
+    @inlineCallbacks
+    def get(self):
+        self.write((yield cyclone.httpclient.fetch("http://bang:10001/requestedTemperature")).body)
     
     
-@bottle.route('/static/<path:path>')
-def server_static(path):
-    return bottle.static_file(path, root='static')
-
-bottle.run(host="0.0.0.0", port=9102)
+if __name__ == '__main__':
+    from twisted.python import log as twlog
+    #twlog.startLogging(sys.stdout)
+            
+    port = 9102
+    reactor.listenTCP(port, cyclone.web.Application(handlers=[
+        (r'/content', Content),
+        (r'/thermostat', Thermostat),        
+        (r'/(.*)', cyclone.web.StaticFileHandler,
+         {"path" : ".", # security hole- serves this dir too
+          "default_filename" : "index.html"}),
+        ]))
+    log.info("serving on %s" % port)
+    reactor.run()
