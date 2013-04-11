d1d3090124890c420da82506c5f4c4d0fe0d4a3c
drewp <drewp@bigasterisk.com>
2013-04-10 21:32:21 -0700

    twilight computation for rules to use
    Ignore-this: c40016acb3c3f957cb17d2b918698c77
    
    darcs-hash:20130411043221-312f9-7af02fd32d1df6a01f151753150d2e4ff87a59c0

--- service/environment/environment.py
+++ service/environment/environment.py
@@ -7,11 +7,13 @@ daytime/night, overall modes like 'maintenance mode', etc
 import sys, datetime, cyclone.web
 from twisted.internet import reactor
 from dateutil.tz import tzlocal
+from dateutil.relativedelta import relativedelta, FR
 from rdflib import Namespace, Literal
 sys.path.append("/my/site/magma")
 from stategraph import StateGraph
 sys.path.append("/my/proj/homeauto/lib")
 from cycloneerr import PrettyErrorHandler
+from twilight import isWithinTwilight
 
 ROOM = Namespace("http://projects.bigasterisk.com/room/")
 DEV = Namespace("http://projects.bigasterisk.com/device/")
@@ -26,6 +28,15 @@ class GraphHandler(PrettyErrorHandler, cyclone.web.RequestHandler):
         now = datetime.datetime.now(tzlocal())
 
         g.add((DEV.environment, ROOM.localHour, Literal(now.hour)))
+
+        for offset in range(-12, 7):
+            d = now.date() + datetime.timedelta(days=offset)
+            if d == d + relativedelta(day=31, weekday=FR(-1)):
+                g.add((DEV.calendar, ROOM.daysToLastFridayOfMonth,
+                       Literal(offset)))
+
+        g.add((DEV.calendar, ROOM.twilight,
+               ROOM['withinTwilight'] if isWithinTwilight(now) else ROOM['daytime']))
         
         self.set_header('Content-type', 'application/x-trig')
         self.write(g.asTrig())
