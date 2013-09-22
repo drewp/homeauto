012d2c32b72b586a92359e021b0234ecf504a409
drewp <drewp@bigasterisk.com>
2013-09-22 00:32:41 -0700

    comment
    Ignore-this: a0c7a9a99558eb8cc97a256a49e8d85a
    
    darcs-hash:20130922073241-312f9-f3cf457b94227246aac822f55a656f99536846f2

--- lib/localdisplay.py
+++ lib/localdisplay.py
@@ -3,6 +3,9 @@ import psutil, os
 def setDisplayToLocalX():
     """
     set DISPLAY env var in this process to the id of the X process on localhost
+
+    I might need something like xauth merge /run/gdm/auth-for-gdm-xxxxxx/database too
+    and this isn't automated yet
     """
     for pid in psutil.get_pid_list():
         try:
