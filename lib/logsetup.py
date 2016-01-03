2e7bd9150518096bd7eea9573a2fcfef30d201e4
drewp@bigasterisk.com <drewp@bigasterisk.com>
2016-01-03 02:28:44 -0800

    enableTwistedLog Ignore-this: 75e28eaa4d1acc2db92144a0620eb9b8

--- lib/logsetup.py
+++ lib/logsetup.py
@@ -5,3 +5,11 @@ logging.basicConfig(format="%(created)f %(asctime)s %(name)s %(levelname)s %(mes
 logging.getLogger('restkit.client').setLevel(logging.WARN)
 log = logging.getLogger()
 log.setLevel(logging.INFO)
+
+def enableTwistedLog():
+    from twisted.python import log as twlog
+    import sys
+    twlog.startLogging(sys.stdout)
+
+    
+
