547f63609701d2f397623a169bd96c11cabe77c6
drewp@bigasterisk.com <drewp@bigasterisk.com>
2016-01-24 07:12:25 -0800

    add new jsonld/SSE support to environment service as a test Ignore-this: ae671e71966dbbb9d1f97e3596802d3d

--- service/environment/environment.py
+++ service/environment/environment.py
@@ -5,62 +5,74 @@ daytime/night, overall modes like 'maintenance mode', etc
 
 """
 import sys, datetime, cyclone.web
-from twisted.internet import reactor
+from twisted.internet import reactor, task
 from dateutil.tz import tzlocal
 from dateutil.relativedelta import relativedelta, FR
 from rdflib import Namespace, Literal
-sys.path.append("/my/site/magma")
-from stategraph import StateGraph
 sys.path.append("/my/proj/homeauto/lib")
+from patchablegraph import PatchableGraph, writeGraphResponse, GraphEventsHandler
 from cycloneerr import PrettyErrorHandler
 from twilight import isWithinTwilight
+
+from rdfdoc import Doc
 
 ROOM = Namespace("http://projects.bigasterisk.com/room/")
 DEV = Namespace("http://projects.bigasterisk.com/device/")
 
+
 class GraphHandler(PrettyErrorHandler, cyclone.web.RequestHandler):
     def get(self):
-        g = StateGraph(ROOM.environment)
-        now = datetime.datetime.now(tzlocal())
+        writeGraphResponse(self, self.settings.masterGraph,
+                           self.request.headers.get('accept'))
 
-        g.add((DEV.environment, ROOM.localHour, Literal(now.hour)))
-        g.add((DEV.environment, ROOM.localTimeToMinute,
-               Literal(now.strftime("%H:%M"))))
-        g.add((DEV.environment, ROOM.localTimeToSecond,
-               Literal(now.strftime("%H:%M:%S"))))
-        g.add((DEV.environment, ROOM.localDayOfWeek,
-               Literal(now.strftime("%A"))))
-        g.add((DEV.environment, ROOM.localMonthDay,
-               Literal(now.strftime("%B %e"))))
-        g.add((DEV.environment, ROOM.localDate,
-               Literal(now.strftime("%Y-%m-%d"))))
+def update(masterGraph):
+    stmt = lambda s, p, o: masterGraph.patchObject(ROOM.environment, s, p, o)
+    
+    now = datetime.datetime.now(tzlocal())
 
-        for offset in range(-12, 7):
-            d = now.date() + datetime.timedelta(days=offset)
-            if d == d + relativedelta(day=31, weekday=FR(-1)):
-                g.add((DEV.calendar, ROOM.daysToLastFridayOfMonth,
-                       Literal(offset)))
+    stmt(DEV.environment, ROOM.localHour, Literal(now.hour))
+    stmt(DEV.environment, ROOM.localTimeToMinute,
+         Literal(now.strftime("%H:%M")))
 
-        g.add((DEV.calendar, ROOM.twilight,
-               ROOM['withinTwilight'] if isWithinTwilight(now) else
-               ROOM['daytime']))
+    stmt(DEV.environment, ROOM.localTimeToSecond,
+         Literal(now.strftime("%H:%M:%S")))
 
-        ct, body = g.asAccepted(self.request.headers.get('accept'))
-        self.set_header('Content-type', ct)
-        self.write(body)
+    stmt(DEV.environment, ROOM.localDayOfWeek,
+         Literal(now.strftime("%A")))
+    stmt(DEV.environment, ROOM.localMonthDay,
+         Literal(now.strftime("%B %e")))
+    stmt(DEV.environment, ROOM.localDate,
+         Literal(now.strftime("%Y-%m-%d")))
 
-from rdfdoc import Doc
-        
-class Application(cyclone.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/()", cyclone.web.StaticFileHandler,
-             {"path": ".", "default_filename": "index.html"}),
-            (r'/graph', GraphHandler),
-            (r'/doc', Doc), # to be shared
-        ]
-        cyclone.web.Application.__init__(self, handlers)
+    for offset in range(-12, 7):
+        d = now.date() + datetime.timedelta(days=offset)
+        if d == d + relativedelta(day=31, weekday=FR(-1)):
+            stmt(DEV.calendar, ROOM.daysToLastFridayOfMonth, Literal(offset))
 
-if __name__ == '__main__':
+    stmt(DEV.calendar, ROOM.twilight,
+         ROOM['withinTwilight'] if isWithinTwilight(now) else ROOM['daytime'])
+
+       
+def main():
+    from twisted.python import log as twlog
+    twlog.startLogging(sys.stderr)
+    masterGraph = PatchableGraph()
+
+    class Application(cyclone.web.Application):
+        def __init__(self):
+            handlers = [
+                (r"/()", cyclone.web.StaticFileHandler,
+                 {"path": ".", "default_filename": "index.html"}),
+                (r'/graph', GraphHandler),
+                (r'/graph/events', GraphEventsHandler),
+                (r'/doc', Doc), # to be shared
+            ]
+            cyclone.web.Application.__init__(self, handlers,
+                                             masterGraph=masterGraph)
+    task.LoopingCall(update, masterGraph).start(1)
     reactor.listenTCP(9075, Application())
     reactor.run()
+
+if __name__ == '__main__':
+    main()
+    
