a3a790353beb2eb8f1d0af962c6ead29f3e5069c
drewp <drewp@bigasterisk.com>
2016-08-19 10:53:03 -0700

    Copy from from https://github.com/juggernaut/twisted-sse-demo
    Ignore-this: a252d3c6e023cb83fd10bfbae78e05d9
    
    darcs-hash:bd76fd3b996c7225f03930b3796dc4ddc7783d9a

---
+++ service/reasoning/twisted_sse_demo/eventsource.py
@@ -0,0 +1,77 @@
+from crochet import setup, run_in_reactor
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
+
+from sse_client import EventSourceProtocol
+
+#setup()
+
+
+class EventSource(object):
+    """
+    The main EventSource class
+    """
+    def __init__(self, url):
+        self.url = url
+        self.protocol = EventSourceProtocol()
+        self.errorHandler = None
+        self.stashedError = None
+        self.connect()
+
+    #@run_in_reactor
+    def connect(self):
+        """
+        Connect to the event source URL
+        """
+        agent = Agent(reactor)
+        d = agent.request(
+            'GET',
+            self.url,
+            Headers({
+                'User-Agent': ['Twisted SSE Client'],
+                'Cache-Control': ['no-cache'],
+                'Accept': ['text/event-stream; charset=utf-8'],
+            }),
+            None)
+        d.addErrback(self.connectError)
+        d.addCallback(self.cbRequest)
+
+    def cbRequest(self, response):
+        print 'cbRequest', response.code
+        if response.code != 200:
+            self.callErrorHandler("non 200 response received: %d" %
+                                  response.code)
+        else:
+            finished = Deferred()
+            self.protocol.setFinishedDeferred(finished)
+            response.deliverBody(self.protocol)
+            return finished
+
+    def connectError(self, ignored):
+        self.callErrorHandler("error connecting to endpoint: %s" % self.url)
+
+    def callErrorHandler(self, msg):
+        if self.errorHandler:
+            func, callInThread = self.errorHandler
+            if callInThread:
+                reactor.callInThread(func, msg)
+            else:
+                func(msg)
+        else:
+            self.stashedError = msg
+
+    def onerror(self, func, callInThread=False):
+        self.errorHandler = func, callInThread
+        if self.stashedError:
+            self.callErrorHandler(self.stashedError)
+
+    def onmessage(self, func, callInThread=False):
+        self.addEventListener('message', func, callInThread)
+
+    def addEventListener(self, event, func, callInThread=False):
+        callback = func
+        if callInThread:
+            callback = lambda data: reactor.callInThread(func, data)
+        self.protocol.addCallback(event, callback)
---
+++ service/reasoning/twisted_sse_demo/sse_client.py
@@ -0,0 +1,62 @@
+from twisted.protocols.basic import LineReceiver
+
+
+class EventSourceProtocol(LineReceiver):
+    def __init__(self):
+        self.callbacks = {}
+        self.finished = None
+        # Initialize the event and data buffers
+        self.event = 'message'
+        self.data = ''
+
+    def setFinishedDeferred(self, d):
+        self.finished = d
+
+    def addCallback(self, event, func):
+        self.callbacks[event] = func
+        
+    def lineReceived(self, line):
+        if line == '':
+            # Dispatch event
+            self.dispatchEvent()
+        else:
+            try:
+                field, value = line.split(':', 1)
+                # If value starts with a space, strip it.
+                value = lstrip(value)
+            except ValueError:
+                # We got a line with no colon, treat it as a field(ignore)
+                return
+
+            if field == '':
+                # This is a comment; ignore
+                pass
+            elif field == 'data':
+                self.data += value + '\n'
+            elif field == 'event':
+                self.event = value
+            elif field == 'id':
+                # Not implemented
+                pass
+            elif field == 'retry':
+                # Not implemented
+                pass
+
+    def connectionLost(self, reason):
+        if self.finished:
+            self.finished.callback(None)
+
+    def dispatchEvent(self):
+        """
+        Dispatch the event
+        """
+        # If last character is LF, strip it.
+        if self.data.endswith('\n'):
+            self.data = self.data[:-1]
+        if self.event in self.callbacks:
+            self.callbacks[self.event](self.data)
+        self.data = ''
+        self.event = 'message'
+
+def lstrip(value):
+    return value[1:] if value.startswith(' ') else value
