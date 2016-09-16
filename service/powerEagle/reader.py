28d8400ba815352bdf24e588460b8ceb63248125
drewp <drewp@bigasterisk.com>
2016-09-16 01:28:48 -0700

    powereagle reader writes to influxdb
    Ignore-this: cbd088f968244a7e869868ff82b079f0
    
    darcs-hash:9a6948cce4adf226fd831d5b3400d54e1ff26c73

--- service/powerEagle/reader.py
+++ service/powerEagle/reader.py
@@ -3,17 +3,15 @@ import json, logging, time
 import sys
 sys.path.append("/my/proj/homeauto/lib")
 from logsetup import log
-sys.path.append("/my/proj/room")
-from carbondata import CarbonClient
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet import reactor
 from cyclone.httpclient import fetch
+from influxdb import InfluxDBClient
 
 from private_config import deviceIp, cloudId, installId, macId, periodSec
 
 auth = (cloudId + ':' + installId).encode('base64').strip()
-
-carbon = CarbonClient(serverHost='bang')
+influx = InfluxDBClient('bang', 9060, 'root', 'root', 'main')
 
 class Poller(object):
     def __init__(self, carbon):
@@ -42,10 +40,16 @@ class Poller(object):
                 raise ValueError
             if ret['summation_units'] != 'kWh':
                 raise ValueError
-            self.carbon.send('system.house.powerEagle.demand_w',
-                             float(ret['demand']) * 1000)
-            self.carbon.send('system.house.powerEagle.summation_delivered_kWh',
-                             float(ret['summation_delivered']))
+            influx.write_points([
+                dict(measurement='housePowerW',
+                     fields=dict(value=float(ret['demand']) * 1000),
+                     tags=dict(house='berkeley'),
+                     time=int(startTime)),
+                dict(measurement='housePowerSumDeliveredKwh',
+                     fields=dict(value=float(ret['summation_delivered'])),
+                     tags=dict(house='berkeley'),
+                     time=int(startTime)),
+                ], time_precision='s')
         except Exception as e:
             log.error("failed: %r", e)
             log.error(repr(ret))
@@ -56,6 +60,7 @@ class Poller(object):
 
 
 log.setLevel(logging.INFO)
-carbon = CarbonClient(serverHost='bang')
-p = Poller(carbon)
+influx = InfluxDBClient('bang', 9060, 'root', 'root', 'main')
+
+p = Poller(influx)
 reactor.run()
