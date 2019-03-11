93efb04e2660426e947b248f01d0dda612966c69
drewp <drewp@bigasterisk.com>
2019-03-11 01:44:13 -0700

    rfid ctypes version starting to work
    Ignore-this: 74e0ddd9f98732739126edfa86398889
    
    darcs-hash:a1caaf84ad844fbf796c4f40ab031615ddd22f27

--- service/rfid_pn532_py/tags_test.py
+++ service/rfid_pn532_py/tags_test.py
@@ -1,13 +1,28 @@
 import unittest
 import tags
+import time
 import os
 os.environ['LIBNFC_DEFAULT_DEVICE'] = "pn532_i2c:/dev/i2c-1"
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger()
 
 class TestNfc(unittest.TestCase):
     def test_open_close(self):
         n = tags.NfcDevice()
         del n
         
-    def test_get_tags(self):
+    def test_update_tag(self): # writes to the current tag!
         n = tags.NfcDevice()
-        print(n.getTags())
+        for t in n.getTags():
+            print('tag', t)
+            print('  tagType', t.tagType())
+            print('  uid %r' % t.uid())
+            t.connect()
+            try:
+                print('  block 1', t.readBlock(1))
+                print('write')
+                t.writeBlock(1, 'hello %s' % int(time.time()))
+                print('  block 1', t.readBlock(1))
+            finally:
+                t.disconnect()
