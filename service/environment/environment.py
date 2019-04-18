618af9f3a15d439142a6ab73dfe7371215e060d3
drewp <drewp@bigasterisk.com>
2019-04-18 09:12:26 -0700

    environment graph service standardization
    Ignore-this: 3aeb86c03902af984a871c9c2d2dd7cd
    
    darcs-hash:d609c91b384e6d59a76babb6666c109653e31dd8

--- service/environment/environment.py
+++ service/environment/environment.py
@@ -4,20 +4,28 @@ return some rdf about the environment, e.g. the current time,
 daytime/night, overall modes like 'maintenance mode', etc
 
 """
-import sys, datetime, cyclone.web
-from twisted.internet import reactor, task
+import sys, datetime, cyclone.web, logging
+from docopt import docopt
+from twisted.internet import reactor, task, defer
 from dateutil.tz import tzlocal
 from dateutil.relativedelta import relativedelta, FR
 from rdflib import Namespace, Literal
-sys.path.append("/my/proj/homeauto/lib")
+from greplin import scales
+from greplin.scales.cyclonehandler import StatsHandler
 from patchablegraph import PatchableGraph, CycloneGraphEventsHandler, CycloneGraphHandler
 from twilight import isWithinTwilight
+from logsetup import log, enableTwistedLog
 
 from rdfdoc import Doc
 
 ROOM = Namespace("http://projects.bigasterisk.com/room/")
 DEV = Namespace("http://projects.bigasterisk.com/device/")
 
+STATS = scales.collection('/root',
+                          scales.PmfStat('update'),
+)
+
+@STATS.update.time()
 def update(masterGraph):
     stmt = lambda s, p, o: masterGraph.patchObject(ROOM.environment, s, p, o)
     
@@ -47,8 +55,17 @@ def update(masterGraph):
 
        
 def main():
-    from twisted.python import log as twlog
-    twlog.startLogging(sys.stderr)
+    arg = docopt("""
+    Usage: environment.py [options]
+
+    -v                    Verbose
+    """)
+    log.setLevel(logging.INFO)
+    if arg['-v']:
+        enableTwistedLog()
+        log.setLevel(logging.DEBUG)
+        defer.setDebugging(True)
+        
     masterGraph = PatchableGraph()
 
     class Application(cyclone.web.Application):
@@ -62,6 +79,7 @@ def main():
                 (r'/graph/events',
                  CycloneGraphEventsHandler, {'masterGraph': masterGraph}),
                 (r'/doc', Doc), # to be shared
+                (r'/stats/(.*)', StatsHandler, {'serverName': 'environment'}),
             ]
             cyclone.web.Application.__init__(self, handlers,
                                              masterGraph=masterGraph)
