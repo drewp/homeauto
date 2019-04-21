194bf86e9f22c0b152b458d2d0b1aab068d02e70
drewp <drewp@bigasterisk.com>
2019-04-20 23:41:34 -0700

    last version of nim attempt for pn532 rfid reader. adds simple nim lib for RDF
    Ignore-this: 7bc9a248f49f57dd0a08c849b807a586
    
    darcs-hash:0972a0a0a58feee13f536cb4352919138e350d71

--- service/rfid_pn532/graphserver.nim
+++ service/rfid_pn532/graphserver.nim
@@ -1,4 +1,7 @@
 import asynchttpserver, asyncdispatch, asyncnet, strtabs, sequtils, times, os, strutils, strformat
+import sets
+import rdf
+import rdf_nodes
 
 type
   GraphServer* = ref object of RootObj
@@ -7,6 +10,20 @@ type
     previousUpdateTime: int
     httpServer: AsyncHttpServer
     serverReady: Future[void]
+    graph: Graph
+
+proc ssePayload(eventType: string, body: string): string =
+  eventType & ": " & body & "\n\n"
+  
+proc sendEvent(client: AsyncSocket, eventType: string, body: string) {.async.} =
+  if not client.isClosed():
+    asyncCheck client.send(ssePayload(eventType, body))
+  
+proc sendEventToAll(self: GraphServer, eventType: string, body: string) {.async.} =
+  let payload = ssePayload(eventType, body)
+  for client in self.clients:
+    if not client.isClosed():
+      asyncCheck client.send(payload)
     
 proc handleCORS(req: Request) {.async.} =
   await req.respond(Http204, "", newHttpHeaders({
@@ -29,6 +46,7 @@ proc handleSSE(self: GraphServer, req: Request) {.async.} =
   await req.client.send("HTTP/1.1 200 OK\c\L")
   await req.sendHeaders(headers)
   await req.client.send("\c\L:ok\n\n")
+  await sendEvent(req.client, "fullGraph", Patch(addQuads: self.graph.stmts).toJson())
   self.clients.add(req.client)
 
 proc handleConnections(self: GraphServer, req: Request) {.async.} =
@@ -41,26 +59,52 @@ proc handleConnections(self: GraphServer, req: Request) {.async.} =
   await req.respond(Http200, $clientCount, headers)
   req.client.close()
 
+proc handleGraph(self: GraphServer, req: Request) {.async.} =
+  await req.respond(Http200, self.graph.toNquads(), newHttpHeaders({
+    "Content-Type": "application/n-quads",
+    }))
+  req.client.close()
+  
+proc applyPatch*(self: GraphServer, p: Patch) {.async.} =
+  self.graph.applyPatch(p)
+  let body = p.toJson()
+  echo "emitpatch " & body
+  asyncCheck self.sendEventToAll("patch", body)
+
+# Replace graph contents. 
+proc setGraph*(self: GraphServer, quads: HashSet[Quad]) {.async.} =
+  let p = Patch(addQuads: quads - self.graph.stmts,
+                delQuads: self.graph.stmts - quads)
+  asyncCheck self.graph.applyPatch(p)
+
+proc handleCurrentGraph(self: GraphServer, req: Request) {.async.} =
+  let quad = HashSet[Quad]([])
+  self.setGraph(quads)
+
+
 proc requestCallback(self: GraphServer, req: Request) {.async.} =
   if req.reqMethod == HttpOptions:
     asyncCheck handleCORS(req)
   else:
     case req.url.path
     of "/connections": asyncCheck self.handleConnections(req)
-    of "/sse": asyncCheck self.handleSSE(req)
+    of "/graph": asyncCheck self.handleGraph(req)
+    of "/graph/events": asyncCheck self.handleSSE(req)
+    of "/currentGraph": asyncCheck self.handleCurrentGraph(req)
     else: asyncCheck handle404(req)
 
 proc newGraphServer*(port: int): GraphServer =
   new(result)
   result.port = port
   result.previousUpdateTime = toInt(epochTime() * 1000)
+  result.graph = initGraph()
 
   result.httpServer = newAsyncHttpServer(true)
   let self = result
-  self.serverReady = self.httpServer.serve(
-    Port(self.port),
-    proc (req: Request): Future[void] = self.requestCallback(req),
-    address="0.0.0.0")
+  # https://github.com/dom96/nim-in-action-code/issues/6#issuecomment-446956468 has been applied to ./nim-0.19.4/lib/pure/asynchttpserver.nim
+  proc handler(req: Request): Future[void] {.async.} =
+    asyncCheck self.requestCallback(req)
+  self.serverReady = self.httpServer.serve(Port(self.port), handler, address="0.0.0.0")
   asyncCheck self.serverReady
   echo "Listening on " & $self.port
   
@@ -74,20 +118,17 @@ proc pingClients(self: GraphServer) {.async.} =
   if currentTime - self.previousUpdateTime < 1000:
     return
 
-  for client in self.clients:
-    if not client.isClosed():
-      asyncCheck client.send("data: " & $currentTime & "\n\n")
-
+  asyncCheck self.sendEventToAll("data", $currentTime)
   self.previousUpdateTime = toInt(epochTime() * 1000)
 
+  
 proc run*(self: GraphServer) =
   while true:
     self.checkClients()
     asyncCheck self.pingClients()
     poll()
 
+   
 
-# Replace graph contents
-proc setGraph*(self: GraphServer) =
-  echo "graph replaced"
-  
+let server = newGraphServer(port = 10012)
+server.run()
