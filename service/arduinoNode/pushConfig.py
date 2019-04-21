8173378c8f79291913bb7c602e7f17b7309e43b2
drewp <drewp@bigasterisk.com>
2019-04-20 23:28:29 -0700

    arduinoNode: build updates, stats support, etc
    Ignore-this: 39386eb7644e3cf522e0f72a874eadba
    
    darcs-hash:b6fa9fbda226917dd3b94d8a87556c4cd6d51180

--- service/arduinoNode/pushConfig.py
+++ service/arduinoNode/pushConfig.py
@@ -4,7 +4,7 @@ import etcd3
 
 from twisted.python.filepath import FilePath
 
-etcd = etcd3.client(host='bang6')
+etcd = etcd3.client(host='bang6', port=9022)
 
 prefix, = sys.argv[1:]
 
@@ -12,6 +12,7 @@ def main():
     existing = set(md.key for v, md in etcd.get_prefix(prefix))
     written = set()
     root = FilePath('config')
+    print 'reading at %s' % root
     for f in root.walk():
         if f.isfile() and f.path.endswith('.n3'):
             n3 = f.getContent()
