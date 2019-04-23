a24dfe53fda1f3f261ade7762e899ffaff2aa9e8
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-04-23 01:29:32 -0700

    move logsetup into new standardservice pkg Ignore-this: 7dbc4224ad2e8beb2534ccb4943e9a27

--- lib/standardservice/logsetup.py
+++ lib/standardservice/logsetup.py
@@ -6,10 +6,16 @@ logging.getLogger('restkit.client').setLevel(logging.WARN)
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
-def enableTwistedLog():
+def enableTwistedLog(yes):
     from twisted.python import log as twlog
     import sys
     twlog.startLogging(sys.stdout)
 
+def verboseLogging(yes):
+    if yes:
+        enableTwistedLog()
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.WARN)
     
 
---
+++ lib/standardservice/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+ 
+setup(
+    name='standardservice',
+    version='0.1.0',
+    packages=['standardservice'],
+    package_dir={'standardservice': ''},
+    requires=[],
+    url='https://projects.bigasterisk.com/standardservice/standardservice-0.1.0.tar.gz',
+    author='Drew Perttula',
+    author_email='drewp@bigasterisk.com',
+)
