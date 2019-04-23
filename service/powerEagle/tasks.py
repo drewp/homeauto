35c525b55e8a58daface5fc4bd29ec62eb8f8d21
drewp <drewp@bigasterisk.com>
2019-04-23 03:57:55 -0700

    update powereagle to py3, be a server with /stats/, save 'price' field
    Ignore-this: 1b738295b503fb7409311ddd637b7a18
    
    darcs-hash:8965d44bb93751bd5639768f8cd1defa15ab1dab

---
+++ service/powerEagle/tasks.py
@@ -0,0 +1,27 @@
+from invoke import task
+
+JOB = 'powerEagle'
+PORT = 10016
+TAG = f'bang6:5000/{JOB.lower()}_x86:latest'
+
+
+@task
+def build_image(ctx):
+    ctx.run(f'docker build --network=host -t {TAG} .')
+
+@task(pre=[build_image])
+def push_image(ctx):
+    ctx.run(f'docker push {TAG}')
+
+@task(pre=[build_image])
+def shell(ctx):
+    ctx.run(f'docker run --rm -it --cap-add SYS_PTRACE --net=host {TAG} /bin/bash', pty=True)
+
+@task(pre=[build_image])
+def local_run(ctx):
+    ctx.run(f'docker run --rm -it -p {PORT}:{PORT} --net=host {TAG} python3 reader.py -v', pty=True)
+
+@task(pre=[push_image])
+def redeploy(ctx):
+    ctx.run(f'sudo /my/proj/ansible/playbook -l bang -t {JOB}')
+    ctx.run(f'supervisorctl -s http://bang:9001/ restart {JOB}_{PORT}')
