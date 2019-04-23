e0c4d3f9848c00c174779f88834822dcdb7aaf69
drewp <drewp@bigasterisk.com>
2019-04-23 00:53:16 -0700

    release 0.3.0
    Ignore-this: 7cc293807866bb502943fc0350773f7e
    
    darcs-hash:690dd0c24759975fa07b71f3102f189060864a75

--- lib/patchablegraph/setup.py
+++ lib/patchablegraph/setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
  
 setup(
     name='patchablegraph',
-    version='0.2.0',
+    version='0.3.0',
     packages=['patchablegraph'],
     package_dir={'patchablegraph': ''},
     install_requires=[
         'cyclone',
         'twisted',
         'rdflib-jsonld >= 0.3',
-        'rdfdb @ https://projects.bigasterisk.com/rdfdb/rdfdb-0.2.0.tar.gz',
+        'rdfdb @ https://projects.bigasterisk.com/rdfdb/rdfdb-0.3.0.tar.gz',
         'scales @ git+http://github.com/drewp/scales.git@448d59fb491b7631877528e7695a93553bfaaa93',
-        'cycloneerr @ https://projects.bigasterisk.com/cycloneerr/cycloneerr-0.2.0.tar.gz',
-        'twisted_sse @ https://projects.bigasterisk.com/twisted_sse/twisted_sse-0.2.0.tar.gz',
+        'cycloneerr @ https://projects.bigasterisk.com/cycloneerr/cycloneerr-0.3.0.tar.gz',
+        'twisted_sse @ https://projects.bigasterisk.com/twisted_sse/twisted_sse-0.3.0.tar.gz',
     ],
-    url='https://projects.bigasterisk.com/patchablegraph/patchablegraph-0.2.0.tar.gz',
+    url='https://projects.bigasterisk.com/patchablegraph/patchablegraph-0.3.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
