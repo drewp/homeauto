6f2e260003b4154d002ce11a2e78363369a09e9b
drewp <drewp@bigasterisk.com>
2019-04-23 01:30:14 -0700

    release 0.2.0
    Ignore-this: 2b9c4c58478d3c23673172ec9765a814
    
    darcs-hash:0c230c933b4207913333b59cfa5a15be0c5dcb9e

--- lib/standardservice/setup.py
+++ lib/standardservice/setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
  
 setup(
     name='standardservice',
-    version='0.1.0',
+    version='0.2.0',
     packages=['standardservice'],
     package_dir={'standardservice': ''},
     requires=[],
-    url='https://projects.bigasterisk.com/standardservice/standardservice-0.1.0.tar.gz',
+    url='https://projects.bigasterisk.com/standardservice/standardservice-0.2.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
