373894de39935f3c4361da4afd5b938793723a29
drewp <drewp@bigasterisk.com>
2019-04-22 23:21:45 -0700

    make cycloneerr release
    Ignore-this: 85a8e1a3e04a38167d3c41ebb4a88483
    
    darcs-hash:7e728ee4899b7242222d4cff300f3dd3646b2bf3

---
+++ lib/cycloneerr/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+ 
+setup(
+    name='cycloneerr',
+    version='0.0.0',
+    packages=['cycloneerr'],
+    package_dir={'cycloneerr': ''},
+    requires=['cyclone'],
+    url='https://projects.bigasterisk.com/cycloneerr/cycloneerr-0.0.0.tar.gz',
+    author='Drew Perttula',
+    author_email='drewp@bigasterisk.com',
+)
