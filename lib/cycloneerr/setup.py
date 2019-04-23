aed0617b9b6d7faf1dd1f0aee911b021194687c4
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-04-23 02:12:07 -0700

    release 0.2.0 Ignore-this: 91607f26358303141f4ce1380002b4f5

--- lib/cycloneerr/setup.py
+++ lib/cycloneerr/setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
  
 setup(
     name='cycloneerr',
-    version='0.1.0',
+    version='0.2.0',
     packages=['cycloneerr'],
     package_dir={'cycloneerr': ''},
     requires=['cyclone'],
-    url='https://projects.bigasterisk.com/cycloneerr/cycloneerr-0.1.0.tar.gz',
+    url='https://projects.bigasterisk.com/cycloneerr/cycloneerr-0.2.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
