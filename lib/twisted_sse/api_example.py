c5e6fb447e8b073db5ec5eb4ebc20b44651ca360
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-04-22 21:58:09 -0700

    mv twisted_sse Ignore-this: a59fadbe80bc4a393b1dab8228e022c1

--- lib/twisted_sse/api_example.py
+++ lib/twisted_sse/api_example.py
@@ -5,7 +5,7 @@ from eventsource import EventSource
 EVENTSOURCE_URL = 'http://localhost:12000/subscribe'
 
 def onmessage(data):
-    print 'Got payload with data %s' % data
+    print('Got payload with data %s' % data)
 
 if __name__ == '__main__':
     eventSource = EventSource(EVENTSOURCE_URL)
