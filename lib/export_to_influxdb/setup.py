c0b33656b39d13988542204bc9531d25c1a6a61f
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-04-25 23:12:39 -0700

    export_to_influxdb to new package Ignore-this: 573ebd40dcc63350bce2bf573f8908c1

---
+++ lib/export_to_influxdb/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(
+    name='export_to_influxdb',
+    version='0.0.0',
+    packages=['export_to_influxdb'],
+    package_dir={'export_to_influxdb': ''},
+    url='https://projects.bigasterisk.com/export-to-influxdb/export_to_influxdb-0.0.0.tar.gz',
+    author='Drew Perttula',
+    author_email='drewp@bigasterisk.com',
+)
