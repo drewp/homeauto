38a07146cb4edef1aed310fdb675eeb033179053
drewp <drewp@bigasterisk.com>
2019-04-25 17:22:39 -0700

    make devices_shared into lib
    Ignore-this: 9c4c0c05413eb2660312862ecc5a743b
    
    darcs-hash:b6eb09e457a7e8c49a9399a463a3ef64e1bae123

---
+++ lib/devices_shared/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+ 
+setup(
+    name='devices_shared',
+    version='0.0.0',
+    packages=['devices_shared'],
+    package_dir={'devices_shared': ''},
+    requires=[
+        'numpy',
+        'imageio',
+        'rdflib',
+    ],
+    url='https://projects.bigasterisk.com/devices_shared/devices_shared-0.0.0.tar.gz',
+    author='Drew Perttula',
+    author_email='drewp@bigasterisk.com',
+)
