07289e8647128104795a9e43739a38503b288f27
drewp <drewp@bigasterisk.com>
2019-04-25 22:59:43 -0700

    switch to setuptools and 'install_requires'
    Ignore-this: ffb0aa596145b0ab6ce9983b399ef58b
    
    darcs-hash:50e2ae0c03d3d1dfaadd112aa5d23171d6b69f7a

--- lib/cycloneerr/setup.py
+++ lib/cycloneerr/setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
  
 setup(
     name='cycloneerr',
     version='0.2.0',
     packages=['cycloneerr'],
     package_dir={'cycloneerr': ''},
-    requires=['cyclone'],
+    install_requires=['cyclone'],
     url='https://projects.bigasterisk.com/cycloneerr/cycloneerr-0.2.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
--- lib/standardservice/setup.py
+++ lib/standardservice/setup.py
@@ -1,11 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
  
 setup(
     name='standardservice',
     version='0.3.0',
     packages=['standardservice'],
     package_dir={'standardservice': ''},
-    requires=[],
     url='https://projects.bigasterisk.com/standardservice/standardservice-0.3.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
