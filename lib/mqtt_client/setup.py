c1fb3bcbe5a274f896c7408ef1c7bddc5c877939
drewp <drewp@bigasterisk.com>
2019-05-01 00:03:23 -0700

    release 0.2.0
    Ignore-this: bccc49bf419bf89714a79d46354f8446
    
    darcs-hash:734f4ac4ad2c99b8088100247118b7bc964ac7a4

--- lib/mqtt_client/setup.py
+++ lib/mqtt_client/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
  
 setup(
     name='mqtt_client',
-    version='0.1.0',
+    version='0.2.0',
     packages=['mqtt_client'],
     package_dir={'mqtt_client': ''},
     install_requires=['rx', 'twisted-mqtt'],
-    url='https://projects.bigasterisk.com/mqtt-client/mqtt_client-0.1.0.tar.gz',
+    url='https://projects.bigasterisk.com/mqtt-client/mqtt_client-0.2.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
