c0f7bd971392ef6ac35ac29d21968c3a4f0cc4e0
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-05-06 00:19:08 -0700

    release 0.5.0 Ignore-this: 4cc6f8ecc1ece80ed147bacdba72426d

--- lib/standardservice/setup.py
+++ lib/standardservice/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
  
 setup(
     name='standardservice',
-    version='0.4.0',
+    version='0.5.0',
     packages=['standardservice'],
     package_dir={'standardservice': ''},
-    url='https://projects.bigasterisk.com/standardservice/standardservice-0.4.0.tar.gz',
+    url='https://projects.bigasterisk.com/standardservice/standardservice-0.5.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
