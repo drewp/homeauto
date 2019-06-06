4d52e394e05cec12f5c9980fef1c3f539f727a93
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-06-06 02:30:00 -0700

    release 0.6.0 Ignore-this: e561ca14b3db890a9a2b996b22fbd959

--- lib/standardservice/setup.py
+++ lib/standardservice/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
  
 setup(
     name='standardservice',
-    version='0.5.0',
+    version='0.6.0',
     packages=['standardservice'],
     package_dir={'standardservice': ''},
     install_requires=[
@@ -10,7 +10,7 @@ setup(
         'twisted',
         'scales', # use git+http://github.com/drewp/scales.git@master#egg=scales
     ],
-    url='https://projects.bigasterisk.com/standardservice/standardservice-0.5.0.tar.gz',
+    url='https://projects.bigasterisk.com/standardservice/standardservice-0.6.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
