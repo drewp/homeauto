a2103bd743c72de160f114f561e6623b6adb0db7
drewp <drewp@bigasterisk.com>
2019-08-12 02:23:15 -0700

    release 0.5.0
    Ignore-this: bfa0bad6d5529a9e3e1051fc1b1c48f6
    
    darcs-hash:32b88b45d0318bcf7a3a81ddbd5e22a95cd7a687

--- lib/devices_shared/setup.py
+++ lib/devices_shared/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
  
 setup(
     name='devices_shared',
-    version='0.4.0',
+    version='0.5.0',
     packages=['devices_shared'],
     package_dir={'devices_shared': ''},
     install_requires=[
@@ -10,7 +10,7 @@ setup(
         'imageio',
         'rdflib',
     ],
-    url='https://projects.bigasterisk.com/devices-shared/devices_shared-0.4.0.tar.gz',
+    url='https://projects.bigasterisk.com/devices-shared/devices_shared-0.5.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
