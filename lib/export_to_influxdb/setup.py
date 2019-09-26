745afafc096bd2aeccd013291236255885418096
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-09-25 17:56:15 -0700

    dep on influxdb Ignore-this: a89f9f27d849d29e6b11234d6556c0b1

--- lib/export_to_influxdb/setup.py
+++ lib/export_to_influxdb/setup.py
@@ -5,6 +5,9 @@ setup(
     version='0.1.0',
     packages=['export_to_influxdb'],
     package_dir={'export_to_influxdb': ''},
+    install_requires=[
+      'influxdb >= 3.0.0'
+    ],
     url='https://projects.bigasterisk.com/export-to-influxdb/export_to_influxdb-0.1.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
