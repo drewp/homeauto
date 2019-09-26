677705a41e80745a870f654b45f28543540d96c5
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-09-25 17:28:38 -0700

    build and release config Ignore-this: a1a0231a2811ac29f906ca72d85a8c34

---
+++ service/store/tasks.py
@@ -0,0 +1,26 @@
+from invoke import task
+
+JOB = 'store'
+PORT = 10015
+
+TAG = f'bang6:5000/{JOB}_x86:latest'
+
+@task
+def build_image(ctx):
+    ctx.run(f'docker build --network=host -t {TAG} .')
+
+@task(pre=[build_image])
+def push_image(ctx):
+    ctx.run(f'docker push {TAG}')
+
+@task
+def shell(ctx):
+    ctx.run(f'docker run --rm -it --cap-add SYS_PTRACE  -v `pwd`:/opt/homeauto_store --dns 10.2.0.1 --dns-search bigasterisk.com --net=host {TAG}  /bin/bash', pty=True)
+
+@task(pre=[build_image])
+def local_run(ctx):
+    ctx.run(f'docker run --rm -it -p {PORT}:{PORT}  -v `pwd`:/opt/homeauto_store --dns 10.2.0.1 --dns-search bigasterisk.com --net=host {TAG} python {JOB}.py -v', pty=True)
+
+@task(pre=[push_image])
+def redeploy(ctx):
+    ctx.run(f'supervisorctl -s http://bang:9001/ restart {JOB}_{PORT}')
