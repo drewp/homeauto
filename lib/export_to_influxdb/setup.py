ce8cedea1f495600261f605c26292a048c9a1bee
drewp <drewp@bigasterisk.com>
2019-09-25 17:56:28 -0700

    release 0.2.0
    Ignore-this: 68d50df5792c57ef558e7636046db7b7
    
    darcs-hash:2a499b0e26f9fe8ef91d93d73777bfd8ace3d5a7

--- lib/export_to_influxdb/setup.py
+++ lib/export_to_influxdb/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
  
 setup(
     name='export_to_influxdb',
-    version='0.1.0',
+    version='0.2.0',
     packages=['export_to_influxdb'],
     package_dir={'export_to_influxdb': ''},
     install_requires=[
       'influxdb >= 3.0.0'
     ],
-    url='https://projects.bigasterisk.com/export-to-influxdb/export_to_influxdb-0.1.0.tar.gz',
+    url='https://projects.bigasterisk.com/export-to-influxdb/export_to_influxdb-0.2.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
