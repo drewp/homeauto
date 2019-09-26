c5425d7f55ae12b18fff3f02489deaff36dae298
drewp <drewp@bigasterisk.com>
2019-09-25 17:29:47 -0700

    graph path now has 'store' in it
    Ignore-this: f20acfa66fa56e5b4e076e76cad140f6
    
    darcs-hash:01a6fa048f8d946b52dbcd7102f236d8ccf01898

--- service/store/store.py
+++ service/store/store.py
@@ -75,8 +75,8 @@ if __name__ == '__main__':
     reactor.listenTCP(port, cyclone.web.Application([
         (r"/()", cyclone.web.StaticFileHandler,
          {"path": ".", "default_filename": "index.html"}),
-        (r"/graph", CycloneGraphHandler, {'masterGraph': masterGraph}),
-        (r"/graph/events", CycloneGraphEventsHandler,
+        (r"/store", CycloneGraphHandler, {'masterGraph': masterGraph}),
+        (r"/store/events", CycloneGraphEventsHandler,
          {'masterGraph': masterGraph}),
         (r'/values', ValuesResource),
     ], masterGraph=masterGraph, dbFile=dbFile, debug=arg['-v']),
