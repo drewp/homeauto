be1424e4caa4090a2bfb41987f1333679a9f8909
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-09-25 17:25:36 -0700

    collector config Ignore-this: 627203d9daf08d043162603abceef88b

--- service/collector/sse_collector_config.py
+++ service/collector/sse_collector_config.py
@@ -3,41 +3,47 @@ config = {
         {'id': 'home',
          'sources': [
              # should be from :reasoning :source ?s
-             'http://garage.vpn-home:9059/graph/events', # "garage pi"
-             'http://kitchen.vpn-home:9059/graph/events', # "kitchen pi"
-             'http://living.vpn-home:9059/graph/events', # "living room pi"
+             'http://garage.vpn-home.bigasterisk.com:9059/graph/events', # "garage pi"
+             'http://kitchen.vpn-home.bigasterisk.com:9059/graph/events', # "kitchen pi"
+             'http://living.vpn-home.bigasterisk.com:9059/graph/events', # "living room pi"
              'http://bang:9059/graph/events', # "bang arduino"
-             'http://bed.vpn-home:9059/graph/events', # "bed pi"
-             'http://changing.vpn-home:9059/graph/events', # "changing pi"
-             'http://frontbed.vpn-home:9059/graph/events', #  "frontbed pi"
-             'http://workshop.vpn-home:9059/graph/events', 
+             'http://bed.vpn-home.bigasterisk.com:9059/graph/events', # "bed pi"
+             'http://changing.vpn-home.bigasterisk.com:9059/graph/events', # "changing pi"
+             'http://frontbed.vpn-home.bigasterisk.com:9059/graph/events', #  "frontbed pi"
+             'http://workshop.vpn-home.bigasterisk.com:9059/graph/events',
              'http://bang:9075/graph/events', # "env"
              'http://bang:9070/graph/events', # "wifi usage"
              #'http://bang:9099/graph/events', # "trails" (big!)
-             'http://dash:9095/graph/events', # "dash monitor"
-             'http://dash:9107/graph/events', # "dash x idle"
-             #'http://slash:9095/graph/events', # "slash monitor"
-             #'http://slash:9107/graph/events', # "slash x idle"
-             'http://frontdoor.vpn-home:9059/graph/events', # frontdoor pi
-             'http://frontdoor.vpn-home:9095/graph/events', #  "frontdoor monitor" 
-             'http://frontdoor.vpn-home:9107/graph/events', #  "frontdoor x idle"
-             'http://frontdoor.vpn-home:10012/graph/events', #  "frontdoor rfid"
-             'http://frontdoor.vpn-home:10013/graph/events', #  "frontdoor tiny screen"
+             'http://dash:9095/graph/dpms/events', # "dash monitor"
+             'http://dash:9107/graph/xidle/events', # "dash x idle"
+             #'http://slash:9095/graph/dpms/events', # "slash monitor"
+             #'http://slash:9107/graph/xidle/events', # "slash x idle"
+             'http://frontdoor.vpn-home.bigasterisk.com:9059/graph/events', # frontdoor pi
+             'http://frontdoor.vpn-home.bigasterisk.com:9095/graph/dpms/events', #  "frontdoor monitor"
+             'http://frontdoor.vpn-home.bigasterisk.com:9107/graph/xidle/events', #  "frontdoor x idle"
+             'http://frontdoor.vpn-home.bigasterisk.com:10012/graph/events', #  "frontdoor rfid"
+             'http://frontdoor.vpn-home.bigasterisk.com:10013/graph/events', #  "frontdoor tiny screen"
+
+             'http://bang:10018/graph/events', #  "frontwindow tag reader"
              'http://bang:10011/graph/events', #  "frontdoor lock"
              'http://bang:10008/graph/events', # kitchen H801
              'http://bang:10015/graph/events', # store
+             'http://bang:10018/mqtt/events', # rdf_from_mqtt
          ]},
         {'id': 'frontDoor',
          'sources': [
-             'http://garage.vpn-home:9059/graph/events', 
-             'http://kitchen.vpn-home:9059/graph/events',
-             'http://living.vpn-home:9059/graph/events', 
-             'http://workshop.vpn-home:9059/graph/events', 
-             'http://bang.vpn-home:9105/graph/events', # calendar
-             'http://bang.vpn-home:9059/graph/events', # "bang arduino"
-             'http://frontbed.vpn-home:9059/graph/events', 
+             'http://garage.vpn-home.bigasterisk.com:9059/graph/events',
+             'http://kitchen.vpn-home.bigasterisk.com:9059/graph/events',
+             'http://living.vpn-home.bigasterisk.com:9059/graph/events',
+             'http://workshop.vpn-home.bigasterisk.com:9059/graph/events',
+             'http://bang:9105/graph/events', # calendar
+             'http://bang:9105/countdownGraph/events', # calendar
+             'http://bang:9059/graph/events', # "bang arduino"
+             'http://frontbed.vpn-home.bigasterisk.com:9059/graph/events',
              'http://bang:10015/graph/events', # store
-             ]},
+             'http://bang:10018/graph/events', #  "frontwindow tag reader"
+             'http://bang:10018/mqtt/events', # rdf_from_mqtt
+         ]},
         {'id': 'network',
          'sources': [
              'http://bang:9070/graph/events', # "wifi usage"
@@ -45,12 +51,13 @@ config = {
          ]},
         {'id': 'source_frontDoor',
          'sources': [
-             'http://frontdoor.vpn-home:9059/graph/events', # frontdoor pi
-             'http://frontdoor.vpn-home:9095/graph/events', #  "frontdoor monitor" 
-             'http://frontdoor.vpn-home:9107/graph/events', #  "frontdoor x idle"
-             'http://frontdoor.vpn-home:10012/graph/events', #  "frontdoor rfid"
-             'http://frontdoor.vpn-home:10013/graph/events', #  "frontdoor tiny screen"
+             'http://frontdoor.vpn-home.bigasterisk.com:9059/graph/events', # frontdoor pi
+             'http://frontdoor.vpn-home.bigasterisk.com:9095/graph/dpms/events', #  "frontdoor monitor"
+             'http://frontdoor.vpn-home.bigasterisk.com:9107/graph/xidle/events', #  "frontdoor x idle"
+             'http://frontdoor.vpn-home.bigasterisk.com:10012/graph/events', #  "frontdoor rfid"
+             'http://frontdoor.vpn-home.bigasterisk.com:10013/graph/events', #  "frontdoor tiny screen"
              'http://bang:10011/graph/events', #  "frontdoor lock"
+             'http://bang:10018/graph/events', #  "frontwindow tag reader"
          ]},
         {'id': 'env',
          'sources': [
@@ -58,8 +65,8 @@ config = {
          ]},
         {'id': 'workshop',
          'sources': [
-             'http://workshop.vpn-home:9059/graph/events', 
+             'http://workshop.vpn-home.bigasterisk.com:9059/graph/events',
          ]},
-        
+
     ]
 }
