92a565fd855710d153c66c1404bb4ac5230fd844
drewp <drewp@bigasterisk.com>
2019-11-24 00:02:42 -0800

    release 0.9.0
    Ignore-this: cd9ac6c22e35bce9d647edd85ab75acd
    
    darcs-hash:ee94646423f37369ac319ecce7a49cb108ecada4

--- lib/homeauto_anynode/setup.py
+++ lib/homeauto_anynode/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
  
 setup(
     name='homeauto_anynode',
-    version='0.8.0',
+    version='0.9.0',
     packages=['homeauto_anynode'],
     package_dir={'homeauto_anynode': ''},
     include_package_data=True,
-    url='https://projects.bigasterisk.com/homeauto-anynode/homeauto_anynode-0.8.0.tar.gz',
+    url='https://projects.bigasterisk.com/homeauto-anynode/homeauto_anynode-0.9.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
