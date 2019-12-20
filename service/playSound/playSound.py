e73cf68f85756c24c760c831f55d4813028425ce
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-12-19 17:33:24 -0800

    drop speech synth, try to get pulse client working Ignore-this: f032ddd2169cbb668c11ed7d48f7de23

--- service/playSound/playSound.py
+++ service/playSound/playSound.py
@@ -3,30 +3,13 @@ play sounds according to POST requests.
 """
 from docopt import docopt
 import cyclone.web
-import sys, tempfile, itertools
+import os, sys, tempfile, itertools
 from twisted.internet import reactor
 from cyclone.httpclient import fetch
-from generator import tts
-import xml.etree.ElementTree as ET
 from twisted.web.static import File
 from standardservice.logsetup import log, verboseLogging
-import os
 
 soundCount = itertools.count()
-
-def makeSpeech(speech, fast=False):
-    speechWav = tempfile.NamedTemporaryFile(suffix='.wav')
-
-    root = ET.Element("SABLE")
-    r = ET.SubElement(root, "RATE",
-                      attrib=dict(SPEED="+50%" if fast else "+0%"))
-    for sentence in speech.split('.'):
-        div = ET.SubElement(r, "DIV")
-        div.set("TYPE", "sentence")
-        div.text = sentence
-
-    speechSecs = tts(speech, speechWav.name)
-    return pygame.mixer.Sound(speechWav.name), speechSecs
 
 class LOADING(object): pass
 
@@ -60,16 +43,6 @@ class SoundEffects(object):
         snd.set_volume(self.volume)
         return self.playBuffer(snd)
 
-    def playSpeech(self, txt, preEffect=None, postEffect=None, preEffectOverlap=0):
-        buf, secs = makeSpeech(txt)
-        t = 0
-        if preEffect:
-            t += self.playEffect(preEffect)
-
-        self.playingSources.append(buf)
-        reactor.callLater(secs + .1, self.done, buf)
-        return secs
-
     def done(self, src):
         try:
             self.playingSources.remove(src)
@@ -86,14 +59,7 @@ class SoundEffects(object):
 
 class Index(cyclone.web.RequestHandler):
     def get(self):
-        self.render('index.html', effectNames=[
-            dict(name=k, postUri='effects/%s' % k)
-            for k in self.settings.sfx.buffers.keys()])
-
-class Speak(cyclone.web.RequestHandler):
-    def post(self):
-        self.settings.sfx.playSpeech(self.get_argument('msg'))
-        return "ok"
+        self.render('index.html')
 
 class PlaySound(cyclone.web.RequestHandler):
     def post(self):
@@ -128,11 +94,9 @@ if __name__ == '__main__':
 
     reactor.listenTCP(9049, cyclone.web.Application(handlers=[
         (r'/', Index),
-        (r'/speak', Speak),
         (r'/playSound', PlaySound),
         (r'/volume', Volume),
         (r'/stopAll', StopAll),
-        (r'/static/(.*)', cyclone.web.StaticFileHandler, {'path': 'static'}),
     ], template_path='.', sfx=sfx))
     reactor.run()
 server.app.run(endpoint_description=r"tcp6:port=9049:interface=\:\:")
