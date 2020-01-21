44dbb2dc7079e2c4e177b401a23649a1f32392f3
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-01-20 17:09:39 -0800

    add task to pull new docker image Ignore-this: f69eb791b4af38e32a165b1443c793e1

--- service/espNode/tasks.py
+++ service/espNode/tasks.py
@@ -1,16 +1,22 @@
 from invoke import task
 
+tag = 'esphome/esphome:dev'
+
+@task
+def pull_esphome(ctx):
+    ctx.run(f"docker pull {tag}")
+
 @task
 def program_board_over_usb(ctx, board):
-    tag = 'esphome/esphome'
-    ctx.run(f"docker run --rm -v `pwd`:/config -v /usr/share/fonts:/usr/share/fonts --device=/dev/ttyUSB1 -it {tag} {board}.yaml run", pty=True)
+    ctx.run(f"docker run --rm -v `pwd`:/config -v /usr/share/fonts:/usr/share/fonts --device=/dev/ttyUSB0 -it {tag} {board}.yaml run", pty=True)
 
 @task
 def program_board_over_wifi(ctx, board):
-    tag = 'esphome/esphome'
     ctx.run(f"docker run --rm -v `pwd`:/config -v /usr/share/fonts:/usr/share/fonts -it --net=host {tag} {board}.yaml run", pty=True)
 
 @task
 def monitor_usb(ctx, board):
-    tag = 'esphome/esphome'
     ctx.run(f"docker run --rm -v `pwd`:/config --device=/dev/ttyUSB0 -it {tag} {board}.yaml logs", pty=True)
+
+# device up?
+#  nmap -Pn -p 3232,6053 10.2.0.21
