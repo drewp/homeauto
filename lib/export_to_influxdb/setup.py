ca5c9a43e168d7d7da43cb370e6937a60d1fddca
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-02-02 14:40:55 -0800

    add scales watching time spent in exportToInflux Ignore-this: 6359e703fdc9704081e6731924d2b61c

--- lib/export_to_influxdb/setup.py
+++ lib/export_to_influxdb/setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
- 
+
 setup(
     name='export_to_influxdb',
     version='0.2.0',
     packages=['export_to_influxdb'],
     package_dir={'export_to_influxdb': ''},
     install_requires=[
-      'influxdb >= 3.0.0'
+        'influxdb >= 3.0.0',
+        'scales',
     ],
     url='https://projects.bigasterisk.com/export-to-influxdb/export_to_influxdb-0.2.0.tar.gz',
     author='Drew Perttula',
