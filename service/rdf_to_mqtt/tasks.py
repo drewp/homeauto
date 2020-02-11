24eddfb2889891557dc4b20602799f70f0cddaec
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-02-10 23:56:42 -0800

    more renaming, build updates Ignore-this: 94a0df55a1d8a74d90ec84bb7351d3ef

--- service/rdf_to_mqtt/tasks.py
+++ service/rdf_to_mqtt/tasks.py
@@ -1,40 +1,26 @@
-from invoke import task
+from invoke import Collection, task
+import sys
+sys.path.append('/my/proj/release')
+from serv_tasks import serv_tasks
 
-JOB = 'rdf_to_mqtt'
-PORT = 10008
-TAG = f'bang6:5000/{JOB}_x86:latest'
+ns = Collection()
+serv_tasks(ns, 'serv.n3', 'rdf_to_mqtt')
 
-@task
-def build_image(ctx):
-    ctx.run(f'docker build --network=host -t {TAG} .')
-
-@task(pre=[build_image])
-def push_image(ctx):
-    ctx.run(f'docker push {TAG}')
-
-@task(pre=[build_image])
-def shell(ctx):
-    ctx.run(f'docker run --name={JOB}_shell --rm -it --cap-add SYS_PTRACE --net=host {TAG} /bin/bash', pty=True)
-
-@task(pre=[build_image])
-def local_run(ctx):
-    ctx.run(f'docker run --name={JOB}_local --rm -it --net=host -v /my/proj/homeauto/lib:/lib_src {TAG} python3 rdf_to_mqtt.py -v', pty=True)
-
-@task(pre=[push_image])
-def redeploy(ctx):
-    ctx.run(f'supervisorctl -s http://bang:9001/ restart {JOB}_{PORT}')
-
+# leftover frontdoor setup I think
+@ns.add_task
 @task
 def program_board_over_usb(ctx):
     tag = 'esphome/esphome'
     ctx.run(f"docker run --rm -v `pwd`:/config --device=/dev/ttyUSB0 -it {tag} door.yaml run", pty=True)
 # config_skylight.yaml run --no-logs
 
+@ns.add_task
 @task
 def monitor_usb(ctx):
     tag = 'esphome/esphome'
     ctx.run(f"docker run --rm -v `pwd`:/config --device=/dev/ttyUSB0 -it {tag} door.yaml logs", pty=True)
 
+@ns.add_task
 @task
 def tail_mqtt(ctx):
     ctx.run(f'mosquitto_sub -h bang -p 10010 -d -v -t \#')
