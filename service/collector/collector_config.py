23aba94ae3d702bceabbff048a8e13dc0123ac8b
drewp <drewp@bigasterisk.com>
2020-02-13 10:22:12 -0800

    update to many new named graphs
    Ignore-this: a887bdab7b280cb5f7dbf2ab59982eaa
    
    darcs-hash:daa39990e8fb5d5a45fa990a9e0bd8a73aee5927

--- service/collector/collector_config.py
+++ service/collector/collector_config.py
@@ -3,45 +3,46 @@ config = {
         {'id': 'home',
          'sources': [
              # should be from :reasoning :source ?s
-             'http://garage5:9059/graph/events', # "garage pi"
-             'http://kitchen5:9059/graph/events', # "kitchen pi"
-             'http://living5:9059/graph/events', # "living room pi"
-             'http://bang:9059/graph/events', # "bang arduino"
-             'http://bed5:9059/graph/events', # "bed pi"
-             'http://changing5:9059/graph/events', # "changing pi"
-             'http://frontbed5:9059/graph/events', #  "frontbed pi"
-             'http://workshop5:9059/graph/events',
-             'http://bang:9075/graph/events', # "env"
-             'http://bang:9070/graph/events', # "wifi usage"
-             #'http://bang:9099/graph/events', # "trails" (big!)
-             'http://dash:9095/graph/dpms/events', # "dash monitor"
-             'http://dash:9107/graph/xidle/events', # "dash x idle"
-             #'http://slash:9095/graph/dpms/events', # "slash monitor"
-             #'http://slash:9107/graph/xidle/events', # "slash x idle"
-             'http://frontdoor5:9059/graph/events', # frontdoor pi
-             'http://frontdoor5:9095/graph/dpms/events', #  "frontdoor monitor"
-             'http://frontdoor5:9107/graph/xidle/events', #  "frontdoor x idle"
-             'http://frontdoor5:10012/graph/events', #  "frontdoor rfid"
-             'http://frontdoor5:10013/graph/events', #  "frontdoor tiny screen"
 
-             'http://bang:10018/mqtt/events', #  "frontwindow tag reader"
-             'http://bang:10011/graph/events', #  "frontdoor lock"
-             'http://bang:10006/timebank/events',
-             'http://bang:10016/power/events',
-             'http://bang:10015/store/events', # store
-             'http://bang:10018/mqtt/events', # rdf_from_mqtt
+             'http://bang:9059/graph/events',
+             'http://bed5:9059/graph/events',
+             'http://changing5:9059/graph/events',
+             'http://frontbed5:9059/graph/events',
+             'http://frontdoor5:9059/graph/events',
+             'http://garage5:9059/graph/events',
+             'http://kitchen5:9059/graph/events',
+             'http://living5:9059/graph/events',
+             'http://workshop5:9059/graph/events',
+
+             #'http://bang:9099/graph/mapTrails/events',
+             'http://slash:9095/graph/dpms/events',
+             'http://slash:9107/graph/xidle/events',
+             'http://dash:9095/graph/dpms/events',
+             'http://dash:9107/graph/xidle/events',
+             'http://frontdoor5:9095/graph/dpms/events',
+             'http://frontdoor5:9107/graph/xidle/events',
+             'http://frontdoor5:10012/graph/rfid/events',
+             'http://frontdoor5:10013/graph/tinyScreen/events',
+
+             'http://bang:9075/graph/environment/events',
+             'http://bang:10011/graph/frontDoorLock/events',
+             'http://bang:10018/graph/mqtt/events',
+             'http://bang:10016/graph/power/events',
+             'http://bang:10015/graph/store/events',
+             'http://bang:10006/graph/timebank/events',
+             'http://bang:9070/graph/wifi/events',
          ]},
-        {'id': 'frontDoor',
+        {'id': 'frontDoor', # used for front door display
          'sources': [
-             'http://bang:10006/timebank/events',
-             'http://bang:10015/store/events',
-             'http://bang:10016/power/events',
-             'http://bang:10018/graph/events', #  "frontwindow tag reader"
-             'http://bang:10018/mqtt/events', # rdf_from_mqtt
-             'http://bang:9059/graph/events', # "bang arduino"
-             'http://bang:9075/graph/events', # "env"
-             'http://bang:9105/countdownGraph/events',
-             'http://bang:9105/graph/events', # calendar
+             'http://bang:9105/graph/calendar/countdown/events',
+             'http://bang:9105/graph/calendar/upcoming/events',
+             'http://bang:9075/graph/environment/events',
+             'http://bang:10018/graph/mqtt/events',
+             'http://bang:10016/graph/power/events',
+             'http://bang:10006/graph/timebank/events',
+             'http://bang:10015/graph/store/events',
+
+             'http://bang:9059/graph/events',
              'http://frontbed5:9059/graph/events',
              'http://garage5:9059/graph/events',
              'http://kitchen5:9059/graph/events',
@@ -50,23 +51,23 @@ config = {
          ]},
         {'id': 'network',
          'sources': [
-             'http://bang:9070/graph/events', # "wifi usage"
-             'http://bang:9073/graph/events', # "dhcpd"
-             'http://bang:9009/graph/traffic/events', # 10.2 traffic
+             'http://bang:9070/graph/wifi/events',
+             'http://bang:9073/graph/dhcpLeases/events',
+             'http://bang:9009/graph/traffic/events',
          ]},
         {'id': 'source_frontDoor',
          'sources': [
-             'http://frontdoor5:9059/graph/events', # frontdoor pi
-             'http://frontdoor5:9095/graph/dpms/events', #  "frontdoor monitor"
-             'http://frontdoor5:9107/graph/xidle/events', #  "frontdoor x idle"
-             'http://frontdoor5:10012/graph/events', #  "frontdoor rfid"
-             'http://frontdoor5:10013/graph/events', #  "frontdoor tiny screen"
-             'http://bang:10011/graph/events', #  "frontdoor lock"
-             'http://bang:10018/graph/events', #  "frontwindow tag reader"
+             'http://frontdoor5:9059/graph/events',
+             'http://frontdoor5:9095/graph/dpms/events',
+             'http://frontdoor5:9107/graph/xidle/events',
+             'http://frontdoor5:10012/graph/rfid/events',
+             'http://frontdoor5:10013/graph/tinyScreen/events',
+             'http://bang:10011/graph/frontDoorLock/events',
+             'http://bang:10018/graph/mqtt/events',
          ]},
         {'id': 'env',
          'sources': [
-             'http://bang:9075/graph/events', # "env"
+             'http://bang:9075/graph/environment/events',
          ]},
         {'id': 'workshop',
          'sources': [
