b89e1c33c7a8965e4b854a64cdd664c156f3c54f
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-02-13 10:16:28 -0800

    build update Ignore-this: 1b29ef44c5f6c3c3e8c70f509fbbde66

--- service/wifi/tasks.py
+++ service/wifi/tasks.py
@@ -1,39 +1,7 @@
-from invoke import task
+from invoke import Collection
+import sys
+sys.path.append('/my/proj/release')
+from serv_tasks import serv_tasks
 
-JOB = 'wifi'
-PORT = 9070
-TAG = f'bang6:5000/{JOB}_x86:latest'
-
-
-@task
-def build(ctx):
-    ctx.run(f'npm run build', pty=True)
-    
-@task(pre=[build])
-def build_image(ctx):
-    ctx.run(f'docker build --network=host -t {TAG} .')
-
-@task(pre=[build_image])
-def push_image(ctx):
-    ctx.run(f'docker push {TAG}')
-
-@task(pre=[build_image])
-def shell(ctx):
-    ctx.run(f'docker run --name {JOB}_shell --rm -it --cap-add SYS_PTRACE --net=host {TAG} /bin/bash', pty=True)
-
-@task(pre=[build_image])
-def local_run(ctx):
-    ctx.run(f'docker run --name {JOB}_local --rm -it --net=host -v `pwd`:/opt {TAG} python3 wifi.py -v', pty=True)
-
-@task(pre=[push_image])
-def redeploy(ctx):
-    ctx.run(f'supervisorctl -s http://bang:9001/ restart {JOB}_{PORT}')
-
-
-# one time:
-#   yarn policies set-version v2
-# and for vscode:
-#   yarn pnpify --sdk
-#   then pick the pnp one on statusbar.
-
-#yarn run webpack-cli --config webpack.config.js --mode production
+ns = Collection()
+serv_tasks(ns, 'serv.n3', 'wifi')
