222649ae7d8030a57c4ae70473b8d1aba604b5fe
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-03-27 22:11:17 -0700

    use local esphome dev version

--- espNode/tasks.py
+++ espNode/tasks.py
@@ -1,6 +1,14 @@
 from invoke import task
 
 tag = 'esphome/esphome:dev'
+tag = 'esphome_dev'
+esphome = f'docker run --rm -v `pwd`:/config -v /usr/share/fonts:/usr/share/fonts --device=/dev/ttyUSB0 -it {tag}'
+
+esphome = '/home/drewp/Downloads/esphome/env/bin/esphome'
+
+@task
+def get_dev_esphome(ctx):
+    ctx.run(f'docker build -t esphome_dev -f docker/Dockerfile https://github.com/MasterTim17/esphome.git#dev')
 
 @task
 def pull_esphome(ctx):
@@ -8,17 +16,20 @@ def pull_esphome(ctx):
 
 @task
 def program_board_over_usb(ctx, board):
+    board = board.replace('.yaml', '')
     print('connect gnd, 3v3, rx/tx per https://randomnerdtutorials.com/esp32-cam-video-streaming-web-server-camera-home-assistant/, ')
     print('rts to reset (if possible), dtr to gpio0 per https://github.com/espressif/esptool/wiki/ESP32-Boot-Mode-Selection#automatic-bootloader')
-    ctx.run(f"docker run --rm -v `pwd`:/config -v /usr/share/fonts:/usr/share/fonts --device=/dev/ttyUSB0 -it {tag} {board}.yaml run", pty=True)
+    ctx.run(f"{esphome} {board}.yaml run", pty=True)
 
 @task
 def program_board_over_wifi(ctx, board):
-    ctx.run(f"docker run --rm -v `pwd`:/config -v /usr/share/fonts:/usr/share/fonts -it --net=host {tag} {board}.yaml run", pty=True)
+    board = board.replace('.yaml', '')
+    ctx.run(f"{esphome} {board}.yaml run", pty=True)
 
 @task
 def monitor_usb(ctx, board):
-    ctx.run(f"docker run --rm -v `pwd`:/config --device=/dev/ttyUSB0 -it {tag} {board}.yaml logs", pty=True)
+    board = board.replace('.yaml', '')
+    ctx.run(f"{esphome} {board}.yaml logs", pty=True)
 
 # device up?
 #  nmap -Pn -p 3232,6053 10.2.0.21
