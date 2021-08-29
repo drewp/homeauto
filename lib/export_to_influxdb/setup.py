b7bef580f964399b42d577ab8871800bd115cb11
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-08-29 13:36:08 -0700

    release 0.5.0

--- lib/export_to_influxdb/setup.py
+++ lib/export_to_influxdb/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='export_to_influxdb',
-    version='0.4.0',
+    version='0.5.0',
     packages=['export_to_influxdb'],
     package_dir={'export_to_influxdb': ''},
     install_requires=[
         'influxdb >= 3.0.0',
         'scales',
     ],
-    url='https://projects.bigasterisk.com/export-to-influxdb/export_to_influxdb-0.4.0.tar.gz',
+    url='https://projects.bigasterisk.com/export-to-influxdb/export_to_influxdb-0.5.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
