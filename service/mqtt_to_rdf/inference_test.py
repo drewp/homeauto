3cc3e506cb0fcfa2d98d28b457b1cf3cd28d5b1e
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-09-02 23:20:55 -0700

    WIP inference- getting into more degenerate test cases

--- service/mqtt_to_rdf/inference_test.py
+++ service/mqtt_to_rdf/inference_test.py
@@ -2,11 +2,63 @@
 also see https://github.com/w3c/N3/tree/master/tests/N3Tests
 """
 import unittest
-
-from rdflib import ConjunctiveGraph, Namespace, Graph
+import itertools
+from rdflib import ConjunctiveGraph, Namespace, Graph, BNode
 from rdflib.parser import StringInputSource
 
 from inference import Inference
+
+
+def patchSlimReprs():
+    import rdflib.term
+
+    def ur(self):
+        clsName = "U" if self.__class__ is rdflib.term.URIRef else self.__class__.__name__
+        return """%s(%s)""" % (clsName, super(rdflib.term.URIRef, self).__repr__())
+
+    rdflib.term.URIRef.__repr__ = ur
+
+    def br(self):
+        clsName = "BNode" if self.__class__ is rdflib.term.BNode else self.__class__.__name__
+        return """%s(%s)""" % (clsName, super(rdflib.term.BNode, self).__repr__())
+
+    rdflib.term.BNode.__repr__ = br
+
+    def vr(self):
+        clsName = "V" if self.__class__ is rdflib.term.Variable else self.__class__.__name__
+        return """%s(%s)""" % (clsName, super(rdflib.term.Variable, self).__repr__())
+
+    rdflib.term.Variable.__repr__ = vr
+
+
+patchSlimReprs()
+
+
+def patchBnodeCounter():
+    import rdflib.term
+    serial = itertools.count()
+
+    def n(cls, value=None, _sn_gen='', _prefix='') -> BNode:
+        if value is None:
+            value = 'N-%s' % next(serial)
+        return rdflib.term.Identifier.__new__(cls, value)
+
+    rdflib.term.BNode.__new__ = n
+
+    import rdflib.plugins.parsers.notation3
+
+    def newBlankNode(self, uri=None, why=None):
+        if uri is None:
+            self.counter += 1
+            bn = BNode('f-%s-%s' % (self.number, self.counter))
+        else:
+            bn = BNode(uri.split('#').pop().replace('_', 'b'))
+        return bn
+
+    rdflib.plugins.parsers.notation3.Formula.newBlankNode = newBlankNode
+
+
+patchBnodeCounter()
 
 ROOM = Namespace('http://projects.bigasterisk.com/room/')
 
@@ -117,10 +169,28 @@ class TestInferenceWithVars(WithGraphEqual):
 
 
 class TestBnodeMatching(WithGraphEqual):
-    def test1(self):
+
+    def testRuleBnodeBindsToInputBnode(self):
         inf = makeInferenceWithRules("{ [ :a :b ] . } => { :new :stmt :here } .")
         implied = inf.infer(N3("[ :a :b ] ."))
         self.assertGraphEqual(implied, N3(":new :stmt :here ."))
+
+    def testRuleVarBindsToInputBNode(self):
+        inf = makeInferenceWithRules("{ ?z :a :b  . } => { :new :stmt :here } .")
+        implied = inf.infer(N3("[] :a :b ."))
+        self.assertGraphEqual(implied, N3(":new :stmt :here ."))
+
+
+class TestSelfFulfillingRule(WithGraphEqual):
+
+    def test1(self):
+        inf = makeInferenceWithRules("{ } => { :new :stmt :x } .")
+        self.assertGraphEqual(inf.infer(N3("")), N3(":new :stmt :x ."))
+        self.assertGraphEqual(inf.infer(N3(":any :any :any .")), N3(":new :stmt :x ."))
+
+    def test2(self):
+        inf = makeInferenceWithRules("{ (2) math:sum ?x } => { :new :stmt ?x } .")
+        self.assertGraphEqual(inf.infer(N3("")), N3(":new :stmt 2 ."))
 
 
 class TestInferenceWithMathFunctions(WithGraphEqual):
@@ -131,9 +201,13 @@ class TestInferenceWithMathFunctions(WithGraphEqual):
         self.assertGraphEqual(inf.infer(N3(":a :b 5 .")), N3(""))
         self.assertGraphEqual(inf.infer(N3(":a :b 6 .")), N3(":new :stmt 6 ."))
 
-    # def testStatementGeneratingRule(self):
-    #     inf = makeInferenceWithRules("{ :a :b ?x . (?x 1) math:sum ?y } => { :new :stmt ?y } .")
-    #     self.assertGraphEqual(inf.infer(N3(":a :b 3 .")), N3(":new :stmt 4 ."))
+    def testStatementGeneratingRule(self):
+        inf = makeInferenceWithRules("{ :a :b ?x . (?x 1) math:sum ?y } => { :new :stmt ?y } .")
+        self.assertGraphEqual(inf.infer(N3(":a :b 3 .")), N3(":new :stmt 4 ."))
+
+    def test3Operands(self):
+        inf = makeInferenceWithRules("{ :a :b ?x . (2 ?x 2) math:sum ?y } => { :new :stmt ?y } .")
+        self.assertGraphEqual(inf.infer(N3(":a :b 2 .")), N3(":new :stmt 6 ."))
 
 
 class TestInferenceWithCustomFunctions(WithGraphEqual):
